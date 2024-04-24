119f108df93d08343e471c1e3ab188de0545ea72
Vikas Tawade <vikas.tawade@remiges.tech>
2024-04-24 15:44:27 +0530

    msg

---
+++ src/ljf/ABC.java
@@ -0,0 +1,12 @@
+package ljf;
+
+public class ABC {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hfiudhfdhv");
+		System.out.println("yoooooooo");
+		System.out.println("hello");
+	}
+
+}
